6c7eb9d7e0e9d3a9941afff410f335d74c849922
hm1996 <humar.moreno@uautonoma.edu.co>
2020-01-30 01:45:49 -0500

    Estructuras Crud y Enviroment Port

---
+++ server/config/config.js
@@ -0,0 +1,1 @@
+process.env.PUERTO = process.env.PUERTO || 1996;
---
+++ server/server.js
@@ -0,0 +1,81 @@
+require('./config/config');
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const puerto = process.env.PUERTO;
+
+
+// CRUD estudiantes
+app.post('/estudiantes', (req, res) => {
+    let body = req.body;
+
+    if(body.correo == undefined){
+        res.status(400).json({
+            status: 400,
+            data: 'Es necesaria la propiedad "correo"'
+        });
+    }else{
+        res.json({
+            status: 200,
+            data: body
+        });
+    }
+
+});
+
+app.get('/estudiantes', (req, res) => {
+    res.json('get estudiantes');
+});
+
+app.put('/estudiantes/:id', (req, res) => {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/estudiantes', (req, res) => {
+    res.json('delete estudiantes');
+});
+
+// CRUD cursos
+app.post('/cursos', (req, res) => {
+    let body = req.body;
+
+    if(body.nombre == undefined){
+        res.status(400).json({
+            status: 400,
+            data: 'Es necesaria la propiedad "nombre"'
+        });
+    }else{
+        res.json({
+            status: 200,
+            data: body
+        });
+    }
+
+});
+
+app.get('/cursos', (req, res) => {
+    res.json('get cursos');
+});
+
+app.put('/cursos/:id', (req, res) => {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/cursos', (req, res) => {
+    res.json('delete cursos');
+});
+
+
+app.listen(puerto, () => {
+    console.log(`Servidor iniciado en el puerto ${puerto}`);
+})
